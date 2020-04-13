035a12e903c3433158f5549fdeac1fc92268503f
sgtmajordoobie <sgtmajordoobie@live.com>
2020-04-13 19:30:18 -0400

    Made the basic layout for the models might have changed something

--- temperature_monitor_app/models.py
+++ temperature_monitor_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+# Sensor registration with settings
+class TemperatureSensors(models.Model):
+    # Alias name as the primary key
+    device_name = models.CharField(max_length=30, primary_key=True)
+
+    # Parameters
+    max_temperature = models.FloatField()
+    min_temperature = models.FloatField()
+
+class LightSensors(models.Model):
+    device_name = models.CharField(max_length=30, primary_key=True)
+
+    # Parameters
+    turn_on_start = models.DateField()
+    turn_on_end = models.DateField()
+
+# History data of sensor statuses
+class TemperatureRead(models.Model):
+    # Alias name of the temperature reader with cascade
+    device_name = models.ForeignKey(TemperatureSensors, on_delete=models.CASCADE)
+    
+    # Timestamp when the record is created
+    sample_date = models.DateField(auto_now=True)
+    
+    # Data from sensors
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+
+    # Able to communicate
+    device_online = models.BooleanField()
+
+class LightRead(models.Model):
+    # Alias name of the temperature reader
+    decice_name = models.ForeignKey(LightSensors, on_delete=models.CASCADE)
+    
+    # Timestamp when the record is created
+    sample_date = models.DateField(auto_now=True)
+
+    # Status of the light
+    powered_on = models.BooleanField()
+
+    # Able to communicate
+    device_online = models.BooleanField()
