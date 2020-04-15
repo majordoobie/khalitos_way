7072a66cb13ed8e64bc57412e83905d7a590c142
majordoobie <sgtmajordoobie@live.com>
2020-04-15 18:54:25 -0400

    Working on the log page

--- temperature_monitor_app/views.py
+++ temperature_monitor_app/views.py
@@ -6,15 +6,17 @@ from django.forms.models import model_to_dict
 from .models import TemperatureRead
 
 def index(request):    
-    last_read = TemperatureRead.objects.latest('sample_date')
-    last_read_dict = {
-        'device': last_read.device_name.device_name,
-        'date': last_read.sample_date.strftime("%d%b%y %H:%M").upper(),
-        'temp': f"{last_read.temperature}\N{DEGREE SIGN}",
-        'humid': f"{last_read.humidity}%",
-        'online': last_read.device_online
-    }
-    return render(request, 'index.html', {'temp_devices': last_read_dict})
+    #last_read = []
+    #last_read.append(TemperatureRead.objects.latest('sample_date'))
+    last_read = TemperatureRead.objects.all()
+    # last_read_dict = {
+    #     'device': last_read.device_name.device_name,
+    #     'date': last_read.sample_date.strftime("%d%b%y %H:%M").upper(),
+    #     'temp': f"{last_read.temperature}\N{DEGREE SIGN}",
+    #     'humid': f"{last_read.humidity}%",
+    #     'online': last_read.device_online
+    # }
+    return render(request, 'monitor/index.html', {'temp_devices': last_read})
 
 
 
